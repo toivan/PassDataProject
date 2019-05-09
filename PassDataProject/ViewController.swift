163776087d32f587d49adac4fc759d646fb2d988
Ivan <toivan777@yahoo.com>
2019-05-09 20:22:24 +0300

    Initial Commit

---
+++ PassDataProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PassDataProject
+//
+//  Created by Иван on 09/05/2019.
+//  Copyright © 2019 Ivan Osipov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
